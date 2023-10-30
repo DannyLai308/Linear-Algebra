4dd74ec23ca4001c06a40505748f1881fc737801
DannyLai308 <56664017+DannyLai308@users.noreply.github.com>
2023-10-29 23:57:23 -0400

    finished calculating for vector, alpha, and e

--- householder.py
+++ householder.py
@@ -10,7 +10,31 @@ def householder(input_matrix):
 
     # Create a result matrix to work on and avoid manipulating original matrix
     result_matrix = input_matrix.copy() 
+    H_matrix = np.identity(m_row)
+    vector = np.zeros_like(result_matrix)
 
+    for k in range (n_column):
+        print(f'**Transformation after column {k+1}:\n')
+        # Extract each column from input matrix as a vector to wokr on
+        current_column = result_matrix[k:, k].astype(float) 
+        e = H_matrix[k:, k] # Get e from identity matrix for calculation of vector
+        if current_column[0] > 0: # Check for sign of a for calculation of alpha
+            alpha = -np.linalg.norm(current_column,2)
+        else:
+            alpha = np.linalg.norm(current_column,2)
+
+        # Calculate vector using current_column vector transpose, alpha, and e
+        vector[k:, k] = np.around(result_matrix[k:, k].T - alpha * e, decimals=0)
+        # Calculate beta using vector transpose and vector
+        beta = np.around(vector[k:, k].T @ vector[k:, k], decimals=0)
+        if beta == 0: # Avoid division by 0 when transforming the remaining submatrix
+            continue
+        
+        print(f'alpha{k+1} = {alpha}, e{k+1} = {e}, beta{k+1} = {beta}\n')
+        print(f'vector{k+1} = {vector[k:, k]}\n')
+
+
+        return result_matrix
 
 M = np.array([[1, -1, 4],
             [1, 4,-2],
