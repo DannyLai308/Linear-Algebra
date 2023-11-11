95225d18d830fe3521c382e564fd290a35b00f76
DannyLai308 <56664017+DannyLai308@users.noreply.github.com>
2023-11-11 16:57:26 -0500

    newtonA to calculate loan repay

---
+++ newton_method.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def X_Y(n):
+    global a, r, p
+    f_n = a*(1+r)**n-p*(((1+r)**n-1)/r)
+    return f_n
+
+def X_Y_derivative(n, h):
+    f_nprime = (X_Y(n+h) - X_Y(n))/h
+    return f_nprime
+
+def NewtonA(n_guess):
+    global h0, a, r, p
+    k = 0
+    while True:
+        if X_Y_derivative(n_guess, h0) == 0:
+            print('Calculation error, please use a different guess for n\n')
+            break
+        h = -X_Y(n_guess)/X_Y_derivative(n_guess, h0)
+        n_next = n_guess + h
+        print(f'{k}\t\t {n_guess:.4f}\t\t\t{X_Y(n_guess):.4f}')
+
+        # Check for error
+        error = abs(n_next - n_guess)/n_next
+        if error <= h0:
+            break
+        k+=1
+        n_guess=n_next
+
+    return n_guess, int(X_Y(n_guess))
+
+a = 100000
+r = 0.06
+p = 10000
+h0 = 1e-6
+n_initial = 10
+print(f'\nInitial guess is n = {n_initial} years\n')
+print(f'k \t\t n\t\t\t\t X-Y')
+n, x_y = NewtonA(n_initial)
+print(f'\nIt will need approximately {np.around(n,decimals=0)} years to repay the loan, when X-Y={x_y}')
